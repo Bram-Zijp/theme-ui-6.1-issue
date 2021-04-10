c70c859dab1b2c97a2deb8f9408456a17dbefced
Bram Zijp <bramzijp1@gmail.com>
2021-04-10 18:05:18 +0200

    fix: somehow it wasn't working anymore

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,6 +1,5 @@
-import React from "react";
-import { ThemeProvider, Box } from "theme-ui";
-import "./button.css";
+import React from 'react';
+import './button.css';
 
 export interface ButtonProps {
   /**
@@ -14,7 +13,7 @@ export interface ButtonProps {
   /**
    * How large should the button be?
    */
-  size?: "small" | "medium" | "large";
+  size?: 'small' | 'medium' | 'large';
   /**
    * Button contents
    */
@@ -28,45 +27,22 @@ export interface ButtonProps {
 /**
  * Primary UI component for user interaction
  */
-export const ButtonX: React.FC<ButtonProps> = ({
+export const Button: React.FC<ButtonProps> = ({
   primary = false,
-  size = "medium",
+  size = 'medium',
   backgroundColor,
   label,
   ...props
 }) => {
-  const mode = primary
-    ? "storybook-button--primary"
-    : "storybook-button--secondary";
+  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
   return (
     <button
       type="button"
-      className={["storybook-button", `storybook-button--${size}`, mode].join(
-        " "
-      )}
+      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
       style={{ backgroundColor }}
       {...props}
     >
-      <Box
-        sx={(theme: any) => {
-          console.log(theme);
-          return {};
-        }}
-      ></Box>
       {label}
     </button>
-  );
-};
-
-export const Button: React.FC<ButtonProps> = (props) => {
-  return (
-    <ThemeProvider
-      theme={{
-        fontSizes: [1, 2, 3],
-        space: [1, 2, 3],
-      }}
-    >
-      <ButtonX {...props} />
-    </ThemeProvider>
   );
 };
