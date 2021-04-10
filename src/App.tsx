e2c845df098c688f80259696a799672e4bf8899f
Bram Zijp <bramzijp1@gmail.com>
2021-04-10 17:18:58 +0200

    fix: this works

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ThemeProvider, Box } from "theme-ui";
+
+const WhatIsTheme = () => {
+  const [state, setState] = React.useState<any>();
+  return (
+    <Box
+      as="pre"
+      sx={(x: any) => {
+        if (!state) {
+          setTimeout(() => {
+            setState(x);
+            console.log(x);
+          });
+        }
+        return {};
+      }}
+    >
+      {JSON.stringify(state, null, 2)}
+    </Box>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={{ fontSizes: [1, 2, 3], space: [1, 2, 3] }}>
+      <WhatIsTheme />
+    </ThemeProvider>
   );
 }
 
